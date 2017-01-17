c0b0db10a310b334e3c48d5f407629c3ebca0622
Tomer Rosenfeld <tomerosenfeld007@gmail.com>
2017-01-17 22:27:30 +0200

    Added project

--- app/src/androidTest/java/com/tomerrosenfeld/fadingtextview/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/tomerrosenfeld/fadingtextview/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.tomer.fadingtextview;
+package com.tomerrosenfeld.fadingtextview;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.tomer.fadingtextview.test", appContext.getPackageName());
+        assertEquals("com.tomerrosenfeld.fadingtextview", appContext.getPackageName());
     }
 }
